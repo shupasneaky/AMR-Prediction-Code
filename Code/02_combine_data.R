eb60141f01f1964cd6b4372f4ef84bfc5d03c98b
Owen Visser <29928411+shupasneaky@users.noreply.github.com>
2025-04-22 15:25:00 -0400

    Delete Code directory

--- Code/02_combine_data.R
+++
@@ -1,8 +0,0 @@
-#setwd(/home/owenvisser/CAMDA2025/fna_data/)
-
-
-fna_codes = unlist(read.delim("Datasets/train_fna.txt", sep='\n', header=F))
-
-
-folder_path = paste0(getwd(),"/Datasets/fna_data/",fna_codes[1],"/")
-tsv_file <- read.delim(paste0(folder_path,fna_codes[1],".tsv"), sep = "\t")
